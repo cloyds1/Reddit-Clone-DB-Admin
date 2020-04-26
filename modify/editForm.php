a0406ad6f47e950fbbf8e17cbff82f23102095bb
cloyds1 <59982570+cloyds1@users.noreply.github.com>
2020-04-25 22:33:57 -0400

    Add files via upload

---
+++ modify/editForm.php
@@ -0,0 +1,47 @@
+<?php 
+
+session_start();
+
+if(!isset($_SESSION['user-data'])){
+	header('Location: ../login/login.php');
+}
+
+if(!is_numeric($_GET['id']) || $_GET['id']<0){
+	
+	header('Location: edit.php');
+	
+}
+
+#import necessary files
+require('../utils/dbUtil.php');
+
+#connect to the database
+$db = mysqli_connect('localhost', 'cloyds1', 'reCxJWbyoUxEx82E', 'redditclonedb');
+
+#get user data
+$user_data = unserialize($_SESSION['user-data']);
+
+#get post at id
+$post = DatabaseUtil::readEntry($_GET['id'], 'posts', $db);
+
+require('../rsrc/header.php');
+
+?>
+
+
+<body>
+<?php require('../rsrc/nav_bar.php'); ?>
+<form class="form-class" action="editHandler.php" method="post">
+	Author User-Name: <?=$user_data->username?><hr>
+	Title:<br><input type="text" name="title" placeholder="Enter text here" size="30" value="<?=$post->title?>"></input><hr>
+	Content:<br><textarea name="content" placeholder="Enter text here"  rows="10" cols="50"><?=$post->content?></textarea><hr>
+	<input type="hidden" name="id" value="<?=$_GET['id']?>">
+	<input type="submit" value="Submit">
+	<input type="reset" value="Reset">
+	
+</form>
+</body
+
+<?php
+require('../rsrc/footer.php');
+?>
